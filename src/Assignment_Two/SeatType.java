425eba73d997c587fcfa033fcd9c8f41f64eeafa
MRLunch <104181831+MRLunch@users.noreply.github.com>
2023-06-06 17:48:13 +1200

    ok

---
+++ src/Assignment_Two/SeatType.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Assignment_Two;
+
+/**
+ *
+ * P08_16932797_21154481
+ */
+
+public enum SeatType
+{
+    BUSINESS,
+    ECONOMY
+}
+
