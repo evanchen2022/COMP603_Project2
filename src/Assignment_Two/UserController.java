4b5db1144d6cd8966f061e7ffeb608a5b21406d8
MRLunch <104181831+MRLunch@users.noreply.github.com>
2023-06-09 15:04:04 +1200

    CommetsUpdated

--- src/Assignment_Two/UserController.java
+++ src/Assignment_Two/UserController.java
@@ -8,18 +8,19 @@ import java.sql.SQLException;
 
 /**
  *
- * @author evan
+ * P08_16932797_21154481
  */
 public class UserController
 {
-
     private UserModel userModel;
 
+    // Constructor of UserController
     public UserController(UserModel userModel)
     {
         this.userModel = userModel;
     }
 
+    // To verify the input username and password
     public boolean verifyUser(String username, String password)
     {
         try
